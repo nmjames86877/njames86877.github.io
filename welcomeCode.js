c1706debb85077a21bd93bff9247895cbd228e99
Nicholas M James <147876915+nmjames86877@users.noreply.github.com>
2024-12-22 00:37:59 -0700

    Update welcomeCode.js

--- welcomeCode.js
+++ welcomeCode.js
@@ -9,7 +9,7 @@ function goToSettings() {
     document.addEventListener('DOMContentLoaded', () => {
         var siriWave = new SiriWave({
             container: document.getElementById('siri-container'),
-            width: 375,
+            width: 400,
             height: 200,
             style: 'ios9',
             speed: 0.2,
